d6fd7f310ea7d56cf718e9bc7d474efd9a132fb5
Usaid <46266811+usaidpeerzada@users.noreply.github.com>
2022-11-27 19:13:46 +0530

    Update cryptoAPI.js

--- src/features/CryptoHomePage/cryptoAPI.js
+++ src/features/CryptoHomePage/cryptoAPI.js
@@ -1,18 +1,42 @@
-// A mock function to mimic making an async request for data
-export function fetchCryptoData(name) {
+export function fetchTop12CryptoData() {
   try {
     let requestOptions = {
       method: "GET",
       redirect: "follow",
     };
     return new Promise((resolve) =>
-      fetch("http://api.coincap.io/v2/markets?limit=12", requestOptions)
+      fetch("https://api.coincap.io/v2/markets?limit=12", requestOptions)
         .then((res) => res.json())
         .then((res) => {
           resolve({ data: res });
         })
         .catch((e) => console.log(e))
     );
+  } catch (error) {
+    console.log("error in fetching data: ", error);
+  }
+}
+export function fetchSearchCryptoData(name) {
+  try {
+    let requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    return new Promise((resolve) => {
+      if (name) {
+        fetch(
+          `https://api.coincap.io/v2/markets?baseId=${name}`,
+          requestOptions
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            resolve({ data: res });
+          })
+          .catch((e) => console.log(e));
+      } else {
+        console.error("name not defined!");
+      }
+    });
   } catch (error) {
     console.log("error in fetching data: ", error);
   }
